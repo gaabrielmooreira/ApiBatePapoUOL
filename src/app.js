54e4a0743a428408089a5bfb680eb2887ea6dfd5
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-12 10:33:53 -0300

    Armazenamento de dados

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
-app.listen(PORT, console.log(`Servidor iniciado na porta ${PORT}`));
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db();
+    console.log("MongoDB connection succesful!");
+} catch (err) {
+    console.log(err.message);
+}
+
+
+
+app.listen(process.env.PORT, console.log(`Servidor iniciado na porta ${process.env.PORT}`));
