c07b0f7ca47fa0a8eff089ecd234744af9861ddd
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-16 00:45:55 -0300

    Validation POST /messages fix

--- src/app.js
+++ src/app.js
@@ -20,42 +20,13 @@ try {
     db = mongoClient.db();
     console.log("MongoDB connection succesful!");
 } catch (err) {
-    console.log(err.message);  
+    console.log(err.message);
 }
-
-app.post("/participants", async (req,res) => {
-    const name = req.body.name;
-    const nameSchema = Joi.object({
-        name: Joi.string().required()
-    });
-    
-    const validation = nameSchema.validate({name});
-    if(validation.error) return res.status(422).send(validation.error.details);
-
-
-    const participantExist = await db.collection("participants").findOne({name});
-    if(participantExist) return res.status(409).send("Name has already been used.");
-    
-    try {
-        await db.collection("participants").insertOne({name, lastStatus: Date.now()});
-        await db.collection("messages")
-            .insertOne({
-                from: name, 
-                to: 'Todos', 
-                text: 'entra na sala...',
-                type: 'status',
-                time: dayjs().format('HH:mm:ss')
-            });
-    } catch(err) {
-        return console.log(err);
-    }
-    res.sendStatus(201);
-})
 
 app.get("/participants", async (req, res) => {
     try {
         const participants = await db.collection("participants").find().toArray();
-        if(participants.length === 0) return res.send("No one participants so far.");
+        if (participants.length === 0) return res.send("No one participants so far.");
         res.send(participants);
     } catch (err) {
         console.log(err);
@@ -64,29 +35,57 @@ app.get("/participants", async (req, res) => {
 
 })
 
-app.post("/messages", async (req,res) => {
-    const from = req.headers.user;
-    const {to, text, type} = req.body;
+app.post("/participants", async (req, res) => {
+    const name = req.body.name;
+    const nameSchema = Joi.object({
+        name: Joi.string().required()
+    });
 
-    const nameExist = await db.collection("participants").findOne({name:from});
-    if(!nameExist) return res.sendStatus(422);
+    const validation = nameSchema.validate({ name });
+    if (validation.error) return res.status(422).send(validation.error.details);
+
+
+    const participantExist = await db.collection("participants").findOne({ name });
+    if (participantExist) return res.status(409).send("Name has already been used.");
+
+    try {
+        await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+        await db.collection("messages")
+            .insertOne({
+                from: name,
+                to: 'Todos',
+                text: 'entra na sala...',
+                type: 'status',
+                time: dayjs().format('HH:mm:ss')
+            });
+    } catch (err) {
+        return console.log(err);
+    }
+    res.sendStatus(201);
+})
+
+app.post("/messages", async (req, res) => {
+    const from = req.headers.user;
+    const { to, text, type } = req.body;
+
+    const nameExist = await db.collection("participants").findOne({ name: from });
+    if (!nameExist) return res.sendStatus(422);
 
     const messageSchema = Joi.object({
         to: Joi.string().required(),
         text: Joi.string().required(),
-        type: Joi.string().valid('message','private_message').required()
+        type: Joi.string().valid('message', 'private_message').required()
     })
 
-    try {
-        messageSchema.validate({to, text, type});
-    } catch (err) {
-        console.log(err);
-        return res.sendStatus(422);
+    const validation = messageSchema.validate({ to, text, type },{abortEarly: false});
+    if (validation.error){
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
     }
 
-    const newMessage = {from, to, text, type};
+    const newMessage = { from, to, text, type };
 
-    db.collection("messages").insertOne({...newMessage, time: dayjs().format("HH:mm:ss")})
+    db.collection("messages").insertOne({ ...newMessage, time: dayjs().format("HH:mm:ss") })
 
     return res.sendStatus(201);
 })
