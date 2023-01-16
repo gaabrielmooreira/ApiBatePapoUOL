25da2e3cc49d8f96d4f48bbe8db64dde5d4eb829
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-16 02:08:39 -0300

    autoRemove usuarios inativos

--- src/app.js
+++ src/app.js
@@ -24,15 +24,15 @@ try {
 }
 
 app.get("/participants", async (req, res) => {
+    const participants = [];
     try {
         const participants = await db.collection("participants").find().toArray();
         if (participants.length === 0) return res.send("No one participants so far.");
-        res.send(participants);
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
-
+    res.send(participants);
 })
 
 app.post("/participants", async (req, res) => {
@@ -67,51 +67,87 @@ app.post("/participants", async (req, res) => {
 app.get("/messages", async (req, res) => {
     const { limit } = req.query;
     const user = req.headers.user;
+    const messages = [];
+    const filterMessages = [];
     try {
-        const messages = await db.collection("messages").find();
+        messages = await db.collection("messages").find();
+        filterMessages = messages.filter((message) => {
+            return (
+                message.from === user || message.to === message.to === 'Todos' || message.to === user
+            )
+        })
+
+        if (filterMessages.length === 0) return res.send("No messages.");
+        if (limit) return res.send(filterMessages.slice(filterMessages.length - limit, filterMessages.length));
+        
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
 
-    const filterMessages = messages.filter((message) => {
-        return (
-            message.from === user || message.to ===message.to === 'Todos' || message.to === user
-        )
-     })
-    
-     if (messages.length === 0) return res.send("No messages.");
-     if (limit) return res.send(filterMessages.slice(filterMessages.length - limit,filterMessages.length));
-     res.send(filterMessages);
+    res.send(filterMessages);
 })
 
 app.post("/messages", async (req, res) => {
     const from = req.headers.user;
     const { to, text, type } = req.body;
-
-    const nameExist = await db.collection("participants").findOne({ name: from });
-    if (!nameExist) return res.sendStatus(422);
-
-    const messageSchema = Joi.object({
-        to: Joi.string().required(),
-        text: Joi.string().required(),
-        type: Joi.string().valid('message', 'private_message').required()
-    })
-
-    const validation = messageSchema.validate({ to, text, type }, { abortEarly: false });
-    if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.message);
-        return res.status(422).send(errors);
-    }
-
-    const newMessage = { from, to, text, type };
     try {
+        const nameExist = await db.collection("participants").findOne({ name: from });
+        if (!nameExist) return res.sendStatus(422);
+
+        const messageSchema = Joi.object({
+            to: Joi.string().required(),
+            text: Joi.string().required(),
+            type: Joi.string().valid('message', 'private_message').required()
+        })
+
+        const validation = messageSchema.validate({ to, text, type }, { abortEarly: false });
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
+
+        const newMessage = { from, to, text, type };
+
         await db.collection("messages").insertOne({ ...newMessage, time: dayjs().format("HH:mm:ss") });
-        return res.sendStatus(201);
     } catch (err) {
         console.log(err);
         return res.sendStatus(500);
     }
+    return res.sendStatus(201);
 })
 
-app.listen(PORT, console.log(`Server started up in PORT: ${process.env.PORT}`));
+app.post("/status", async (req, res) => {
+    const name = req.headers.user;
+    try {
+        const result = await db.collection("participants").updateOne({ name }, { $set: { name, lastStatus: Date.now() } })
+        if (result.modifiedCount === 0) return res.sendStatus(404);
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
+    res.send("OK");
+})
+
+async function autoRemove() {
+    
+    const participants = await db.collection.find();
+
+    const namesToRemove = participants.forEach((p) => {
+        if((Date.now - p.lastStatus) >= 10000) return p.name; 
+    })
+
+    db.collection("participants").deleteMany({name: {$in: namesToRemove}});
+    namesToRemove.map((name) => db.collection("messages")
+    .insertOne({
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
+    }));
+}
+
+setInterval(autoRemove, 15000);
+
+app.listen(PORT, console.log(`Server started up in PORT: ${PORT}`));
